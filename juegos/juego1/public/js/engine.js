87fc892fd378aabb2a733b69d34f8b27da49656a
Gildardo Rosas <gildardo.rosas96@gmail.com>
2018-04-15 14:24:11 -0700

    Comentarios

--- juegos/juego1/public/js/engine.js
+++ juegos/juego1/public/js/engine.js
@@ -10,6 +10,12 @@ var call = function(callback) {
 }
 call();
 
+/**
+ * Verifica que la respuesta sea correcta.
+ * @param int 	   nivel     Indice del nivel de dificultad
+ * @param int      pregunta  Indice de la pregunta dentro del nivel
+ * @param string   respuesta Respuesta a comparar
+ */
 function Responder(nivel, pregunta, respuesta) {
 	resp = config["niveles"][nivel][pregunta]["respuesta"];
 	return resp == respuesta;
