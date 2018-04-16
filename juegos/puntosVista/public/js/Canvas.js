afc7508b40f45c5711ee08d083cf3637eab2d424
Rafael <rafael.peralta.blanco@uabc.edu.mx>
2018-04-16 11:49:47 -0700

    Puntos de vista

---
+++ juegos/puntosVista/public/js/Canvas.js
@@ -0,0 +1,13 @@
+class Img extends React.Component{
+
+  componentDidMount(){
+    const img = this.refs.image
+  }
+
+  render(){
+    return(
+      <img ref="image" src={this.props.url} />
+    )
+  }
+}
+
