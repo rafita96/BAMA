0dbe4ac04c5f301be681a38090796c8cc2678ba3
Gildardo Rosas <gildardo.rosas96@gmail.com>
2018-04-15 20:49:41 -0700

    Juego de los planetas (no terminado)

--- juegos/juego1/public/js/main.js
+++ juegos/juego1/public/js/main.js
@@ -1,27 +1,57 @@
-class Comentario extends React.Component{
+class Image extends React.Component {
+	render() {
+		return (
+			<div className="col-lg-3 col-md-4 col-xs-6">
+				<a href="#" className="d-block mb-4 h-100">
+					<img className="rounded img-fluid img-thumbnail" src={this.props.value} alt={this.props.value} />
+				</a>
+			</div>
+		);
+	}
+}
+
+class Canvas extends React.Component {
+	renderImage(i) {
+		return <Image value={i} />;
+	}
 
 	render() {
-		return <h1>{this.props.children}</h1>;
+		const points = "Puntos acumulados: 0";
+		const title = "¿En qué planeta estamos?"
+
+		return (
+			<div>
+				<div className="title">{title}</div>
+				<div className="points">{points}</div>
+				<div className="row text-center text-lg-left">
+					{this.renderImage("./data/img/mercury.png")}
+					{this.renderImage("./data/img/venus.png")}
+					{this.renderImage("./data/img/earth.png")}
+					{this.renderImage("./data/img/mars.png")}
+					{this.renderImage("./data/img/jupiter.png")}
+					{this.renderImage("./data/img/saturn.png")}
+					{this.renderImage("./data/img/uranus.png")}
+				</div>
+			</div>
+		);
 	}
 
 }
 
-d3.json("./data/texto.json", function(error, data){
-	// Consulta.post('/database/insertar/', { collection: 'rafael', data: {
-	// 	nombre: 'carlos',
-	// 	edad: 7
-	// } }, function(error){
- //        if(error){
- //            console.log("Error");
- //        }
- //    });
+class Game extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-canvas">
+					<Canvas />
+				</div>
 
-    // Consulta.put('/database/actualizar/', {collection: 'rafael', query: {nombre: 'satan'}, data: {edad: 5}} ,function(error){
-    //     if(error){
-    //         console.log("Error");
-    //     }
-    // });
+				<div className="game-info">
+					<div>{ /* puntaje */}</div>
+				</div>
+			</div>
+		);
+	}
+}
 
-
-	ReactDOM.render(<Comentario>{data["texto"]}</Comentario>, document.getElementById('main'));
-});
+ReactDOM.render(<Game />, document.getElementById('main'));
