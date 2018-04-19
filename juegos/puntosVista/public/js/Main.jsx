61b75c5df65e7d9ee119a36a4cdfaa811c24697c
Rafael <rafael.peralta.blanco@uabc.edu.mx>
2018-04-18 17:24:16 -0700

    Extension para jsx

--- juegos/puntosVista/public/js/Main.jsx
+++ juegos/puntosVista/public/js/Main.jsx
@@ -61,29 +61,3 @@ class Main extends React.Component{
 
     }
 }
-
-function getInfo(callback){
-    d3.json("./data/info.json", function(error, instrucciones){
-        d3.json("./meta.json", function(error, nombre){
-            callback(nombre["nombre"], instrucciones);
-        });
-    });
-}
-
-$(document).ready(function(){
-    Consulta.get('/paciente/actual/', function(data){
-        if(data["id"] != null){
-            mostrarPerfil(data);
-            getInfo(function(nombre, instrucciones){
-
-                ReactDOM.render(<Main
-                    paciente={data["id"]}  
-                    nombre={nombre} 
-                    instrucciones={instrucciones["instrucciones"]}
-                    parte2={instrucciones["parte2"]} />, document.getElementById('main'));
-            });
-        }else{
-            toastr("No has seleccionado un paciente");
-        }
-    });
-}); 
