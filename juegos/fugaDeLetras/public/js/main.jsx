c42f3b0ce8a1fae5fde924e083d93baadc750ca5
Manuel Carrillo <mdj.carleo@gmail.com>
2018-04-24 15:00:37 -0700

    agregue unas cosas

--- juegos/fugaDeLetras/public/js/main.jsx
+++ juegos/fugaDeLetras/public/js/main.jsx
@@ -1,37 +1,84 @@
-
 class Main extends React.Component {
 	constructor(props){
         super(props);
         // El inicio debe estar en false para mostrar las instrucciones
         this.state = {
-            inicio: true,
+            seleccionNivel: true,
+            inicio: false,
+            fin: false,
+            porcentaje: null
         }
 
         this.iniciar = this.iniciar.bind(this);
+        this.terminar = this.terminar.bind(this);
+        this.reiniciar = this.reiniciar.bind(this);
+        this.seleccionarNivel = this.seleccionarNivel.bind(this);
+    }
+
+    reiniciar(){
+        this.setState({
+            inicio: false,
+            fin: false,
+            seleccionNivel: true,
+            porcentaje: null
+        });
     }
 
     iniciar(){
         this.setState({
             inicio: true
         });
+        this.fechaInicio = new Date();
+    }
+
+
+    terminar(porcentaje){
+        this.setState({
+            fin: true,
+            porcentaje: porcentaje
+        });
+    }
+
+    seleccionarNivel(nivel){
+        this.nivel = nivel;
+        this.setState({
+            seleccionNivel: false
+        });
     }
 
 	render() {
-		if(this.state.inicio){
-	    	return(
-	        	<Bloque nombre={this.props.nombre}>
-	            	<Ejercicio datos={this.props.datos} />
-	            </Bloque>
-	        );
-		}else{
-	    	return(
-		        <Bloque nombre={this.props.nombre}>
-	    			<Instrucciones 
-						iniciar={this.iniciar} 
-	                    instrucciones={this.props.instrucciones} />
-				</Bloque>
-	        );
-	    }
+        if(this.state.seleccionNivel){
+            return(
+                <Bloque nombre={this.props.nombre}>
+                    <Nivel seleccionar={this.seleccionarNivel} />
+                </Bloque>
+            );
+        }else if(this.state.fin){
+            return(
+                <Bloque nombre={this.props.nombre}>
+                    <Fin 
+                        fechaInicio={this.fechaInicio} 
+                        nivel={this.nivel} 
+                        paciente={this.props.paciente} 
+                        reiniciar={this.reiniciar} 
+                        porcentaje={this.state.porcentaje} />
+                </Bloque>
+            );
+        }else if(this.state.inicio){
+            return(
+                <Bloque nombre={this.props.nombre}>
+                    <Ejercicio nivel={this.nivel} terminar={this.terminar} parte2={this.props.parte2} />
+                </Bloque>
+            );
+        }else{
+            return(
+                <Bloque nombre={this.props.nombre}>
+                    <Instrucciones 
+                        iniciar={this.iniciar} 
+                        instrucciones={this.props.instrucciones} />
+                </Bloque>
+            );
+        }
 	}
 }
 
