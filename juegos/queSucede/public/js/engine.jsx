ea5ac969adf8c7ddea110ae72301c09deed58424
Gildardo Rosas <gildardo.rosas96@gmail.com>
2018-05-02 13:55:02 -0700

    Juego de que sucede

---
+++ juegos/queSucede/public/js/engine.jsx
@@ -0,0 +1,40 @@
+var config;
+
+var call = function(callback) {
+	d3.json("./data/config.json", (error, data) => {
+		if (error) {
+			console.log(error);
+		}
+		config = data;
+	});
+}
+call();
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+/**
+ * Verifica que la respuesta sea correcta.
+ * @param int 	   nivel     Indice del nivel de dificultad
+ * @param int      pregunta  Indice de la pregunta dentro del nivel
+ * @param array    respuesta Respuesta a comparar
+ */
+function Responder(nivel, pregunta, respuesta) {
+	resp = config["niveles"][nivel][pregunta]["respuesta"];
+	resp.sort();
+	respuesta.sort();	
+
+	return JSON.stringify(resp) == JSON.stringify(respuesta);
+}
