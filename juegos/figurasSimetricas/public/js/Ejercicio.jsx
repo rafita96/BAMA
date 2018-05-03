b836f1fe32b2cf5c13038224e8bfd3bb3d6db21f
Manuel Carrillo <mdj.carleo@gmail.com>
2018-05-02 21:09:11 -0700

    figuras simetricas

---
+++ juegos/figurasSimetricas/public/js/Ejercicio.jsx
@@ -0,0 +1,148 @@
+class Ejercicio extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pregunta: 0,
+			aciertos: 0,
+			seleccionado: null
+		}
+		this.generarEjercicios();
+		this.siguiente = this.siguiente.bind(this);
+		this.seleccionar = this.seleccionar.bind(this);
+	}
+
+	seleccionar(seleccionado) {
+		this.setState({
+			seleccionado: seleccionado
+		});
+	}
+
+	generarCadena(figura,color,lado){
+		var cadena = figura+"_"+color;
+		if (figura != "cuadrado")
+			cadena += "_"+lado; 
+		return cadena
+	}
+
+	generarEjercicios(){
+		this.ejercicios = [];
+		for (var i=0;i<5;i++){
+			var colores = ["rojo","azul","verde","naranja","amarillo"];
+			var figuras = ["circulo","triangulo","cuadrado","pentagono","estrella"];
+			var lados = ["der","izq"];
+			var ejercicio = new Object();
+
+			var color_c = colores.splice(Math.floor(Math.random()*colores.length),1)[0];
+			var figura_o = figuras.splice(Math.floor(Math.random()*figuras.length),1)[0];
+			var rand = Math.round(Math.random());
+			var lado_ob = lados[rand];
+			var lado_op = lados[1-rand];
+
+			ejercicio.pregunta = this.generarCadena(figura_o,color_c,lado_ob);
+			if (this.props.nivel != 3)
+				ejercicio.respuesta = this.generarCadena(figura_o,color_c,lado_op);
+			else {
+				color_c = colores.splice(Math.floor(Math.random()*colores.length),1)[0];
+				ejercicio.respuesta = this.generarCadena(figura_o,color_c,lado_op);
+			}
+
+			if (this.props.nivel == 1)
+				var color = colores.splice(Math.floor(Math.random()*colores.length),1)[0];
+			var opciones = [ejercicio.respuesta];
+			for (var j=1; j<=3; j++){
+				if (this.props.nivel == 1){
+					var fig = figuras[Math.floor(Math.random()*figuras.length)];
+				} else if (this.props.nivel == 2){
+					var fig = figuras.splice(Math.floor(Math.random()*figuras.length),1)[0];
+					var color = colores[Math.floor(Math.random()*colores.length)];
+				} else{
+					var fig = figuras.splice(Math.floor(Math.random()*figuras.length),1)[0];
+					var color = colores.splice(Math.floor(Math.random()*colores.length),1)[0];
+				}
+				var lado = lados[Math.round(Math.random())];
+				opciones.push(this.generarCadena(fig,color,lado));
+			}
+
+			opciones = shuffle(opciones);
+			ejercicio.opciones = opciones;
+			this.ejercicios.push(ejercicio);
+		}
+		console.log(this.ejercicios);
+	}
+
+	siguiente() {
+		if (this.state.seleccionado == null) {
+			toastr("No has seleccionado una opción");
+		} else {
+			if (this.state.seleccionado == this.ejercicios[this.state.pregunta].respuesta) {
+				this.setState({
+					aciertos: this.state.aciertos + 1,
+					pregunta: this.state.pregunta + 1,
+					seleccionado: null
+				});
+			} else {
+				this.setState({
+					pregunta: this.state.pregunta + 1,
+					seleccionado: null
+				});
+			}
+		}
+	}
+
+	render() {
+		if (this.state.pregunta >= this.ejercicios.length) {
+			var porcentaje = this.state.aciertos / this.state.pregunta * 100;
+			this.props.terminar(porcentaje);
+            return(<div></div>);
+		} else {
+			var carpeta = this.state.pregunta;
+			return (
+				<div>
+					<div className="row text-center">
+					    <div className="col-12">
+					        <Img url={"./img/"+this.ejercicios[this.state.pregunta].pregunta+".png"} />
+					    </div>    
+					</div>
+					<div className="col-12"><hr/></div>
+					<div className="row text-center"> 
+						<div className="col">
+						    <Img 
+						    	url={"./img/"+this.ejercicios[this.state.pregunta].opciones[0]+".png"} 
+						    	id={this.ejercicios[this.state.pregunta].opciones[0]} 
+						    	seleccionado={this.state.seleccionado}
+						    	seleccionar={this.seleccionar} />
+						</div>
+						<div className="col">
+						    <Img 
+						    	url={"./img/"+this.ejercicios[this.state.pregunta].opciones[1]+".png"} 
+						    	id={this.ejercicios[this.state.pregunta].opciones[1]} 
+						    	seleccionado={this.state.seleccionado}
+						    	seleccionar={this.seleccionar} />
+						</div>
+						<div className="col">
+						    <Img 
+						    	url={"./img/"+this.ejercicios[this.state.pregunta].opciones[2]+".png"} 
+						    	id={this.ejercicios[this.state.pregunta].opciones[2]} 
+						    	seleccionado={this.state.seleccionado}
+						    	seleccionar={this.seleccionar} />
+						</div>
+						<div className="col">
+						    <Img 
+						    	url={"./img/"+this.ejercicios[this.state.pregunta].opciones[3]+".png"} 
+						    	id={this.ejercicios[this.state.pregunta].opciones[3]} 
+						    	seleccionado={this.state.seleccionado}
+						    	seleccionar={this.seleccionar} />
+						</div>	
+					</div>
+
+					<div className="row mt-3">
+						<div className="col-2 offset-10">
+							<button className="btn btn-principal" onClick={this.siguiente}>Siguiente</button>
+						</div>
+					</div>
+				</div>
+			);
+		}
+	}	
+}
---
+++ juegos/figurasSimetricas/public/js/Nivel.jsx
@@ -0,0 +1,64 @@
+class Nivel extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            index: 0
+        }
+
+        this.seleccionar = this.seleccionar.bind(this);
+    }
+
+    seleccionar() {
+        if (this.state.index == 0) {
+            toastr("No has seleccionado un nivel.");
+        } else {
+            this.props.seleccionar(this.state.index);
+        }
+    }
+
+    render() {
+        var niveles = [];
+        for(var i = 0; i < 3; i++){
+            if(this.state.index == i+1) {
+                niveles.push("col-4 bg-verde border text-white py-3");
+            } else {
+                niveles.push("col-4 bg-light border py-3")
+            }
+        }
+
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-6 offset-3 text-center">
+                        <h3>Selecciona el nivel de dificultad</h3>
+                    </div>
+                </div>
+
+                <div className="row text-center">
+                    <div className={niveles[0]} onClick={()=>{this.setState({ index: 1 })}}>Fácil</div>
+
+                    <div className={niveles[1]} onClick={()=>{this.setState({ index: 2 })}}>Medio</div>
+
+                    <div className={niveles[2]} onClick={()=>{this.setState({ index: 3 })}}>Difícil</div>
+                </div>
+
+                <div className="row">
+                    <div className="col-4 text-center mt-3">
+                        <a
+                            className="btn btn-principal btn-lg" 
+                            href="/juegos/">
+                            Lista de juegos
+                        </a>
+                    </div>
+
+                    <div className="col-4 offset-4 text-center mt-3">
+                        <button onClick={this.seleccionar} className="btn btn-principal">Continuar</button>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
