e549869e5d6eb0c90ba91732bedd81879ee32728
Rafael <rafael.peralta.blanco@uabc.edu.mx>
2018-05-11 14:12:53 -0700

    sigueme

---
+++ public/common/js/juegos/Instrucciones.jsx
@@ -0,0 +1,40 @@
+class Instrucciones extends React.Component {
+	render() {
+		var regresar;
+        if(typeof this.props.regresar === 'undefined'){
+            regresar = (
+                <div className="col-4">
+                    <a
+                        className="btn btn-principal btn-lg" 
+                        href="/juegos/">
+                        Lista de juegos
+                    </a>
+                </div>
+            );
+        }else{
+            regresar = (
+                <div className="col-4"></div>
+            );
+        }
+		return (
+			<div>
+				<div className="row border rounder my-3">
+					<div className="col-12 text-justify bg-white">
+						<p>{this.props.instrucciones}</p>
+					</div>
+				</div>
+
+				<div className="row">
+					{regresar}
+					<div className="col-4 text-center">
+						<button 
+							className="btn btn-principal btn-lg"
+							onClick={this.props.iniciar}>
+							Iniciar Juego
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
