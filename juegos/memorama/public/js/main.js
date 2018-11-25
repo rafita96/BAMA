ec706fc086f6ed6aa9aa78444f410d39268ad9ee
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail.com>
2018-11-25 08:01:43 -0800

    Memorama

--- juegos/memorama/public/js/main.js
+++ juegos/memorama/public/js/main.js
@@ -4,7 +4,7 @@ class Fin extends React.Component{
         super(props);
 
         Consulta.post('/paciente/registrar/avance/', {
-                juego: 'dibujaPalabra',
+                juego: 'memorama',
                 paciente: this.props.paciente,
                 porcentaje: this.props.porcentaje,
                 nivel: this.props.nivel,
@@ -113,185 +113,113 @@ class Nivel extends React.Component{
 
 class Ejercicio extends React.Component {
 	constructor(props) {
-
-		super(props);
-		this.state = {
-			pregunta: 0,
-			aciertos: 0,
-			index: null,
-            palabra_index : null,
-            used : [],
-            paint: false,
-            path: []
-		}
-        this.total_preguntas = 5;
-		this.siguiente = this.siguiente.bind(this);
-        this.seleccionar = this.seleccionar.bind(this);
-        this.dibujar = this.dibujar.bind(this);
-        this.addClick = this.addClick.bind(this);
-        this.clear = this.clear.bind(this);
-        this.redraw = this.redraw.bind(this);
-        this.continuarDibujo = this.continuarDibujo.bind(this);
-        this.clean = this.clean.bind(this);
-	}
-
-	seleccionar(index) {
-		this.setState({
-			index: index
-		});
+        super(props);
+        this.n = 4;
+        this.generarCartas = this.generarCartas.bind(this);
+        this.generarCarta = this.generarCarta.bind(this);
+        this.revisar = this.revisar.bind(this);
+        var cartas = this.generarCartas();
+        var activo = [];
+        for (var i = 0; i < this.n * this.n; i++) {
+            activo.push(false);
+        }
+        this.state = {
+            cartas: cartas,
+            activo: activo,
+            selected: -1,
+            done: [],
+            movimientos: 0,
+            locked: false
+        }
     }
     
-    addClick(x, y, drag) {
-        console.log(x, y, drag);
-        this.setState({
-            path: this.state.path.concat({
-                x: x,
-                y: y,
-                drag: drag
-            })
-        });
-    }
-
-    dibujar(event) {
-        var rect = event.target.getBoundingClientRect();
-        var x = event.clientX - rect.left;
-        var y = event.clientY - rect.top;
-        this.setState({
-            paint: true
-        });
-        this.addClick(x, y);
-        this.redraw();
-    }
-
-    clean () {
-        var canvas = document.querySelector('#canvas');
-        var context = canvas.getContext('2d');
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-        this.setState({
-            path: []
-        });
-    }
-
-    redraw() {
-        var canvas = document.querySelector('#canvas');
-        var context = canvas.getContext('2d');
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-        
-        context.strokeStyle = "#df4b26";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
-                  
-        for(var i=0; i < this.state.path.length; i++) {
-            context.beginPath();
-            if (this.state.path[i].drag && i) {
-                context.moveTo(this.state.path[i-1].x, this.state.path[i-1].y);
-            } else {
-                context.moveTo(this.state.path[i].x - 1, this.state.path[i].y);
+    generarCartas() {
+        var cartas = [];
+        var disponibles = ['aguacate', 'banana', 'fresa', 'manzana', 'naranja', 'pera', 'pina', 'zanahoria'];
+        var self = this;
+        disponibles.forEach(function(disponible) {
+            for (var i = 0; i < 2; i++) {
+                var carta = self.generarCarta(cartas);
+                cartas[carta] = disponible;
             }
-            context.lineTo(this.state.path[i].x, this.state.path[i].y);
-            context.closePath();
-            context.stroke();
-        }
-      }
-
-    continuarDibujo(event) {
-        if (!this.state.paint) return;
-        var rect = event.target.getBoundingClientRect();
-        var x = event.clientX - rect.left;
-        var y = event.clientY - rect.top;
-        this.addClick(x, y, true);
-        this.redraw();
-    }
-
-    clear() {
-        this.setState({
-            paint: false
         });
+        return cartas;
     }
 
-	siguiente() {
-		if (this.state.index == null) {
-			toastr("No has seleccionado una opción");
-		} else {
-            var aciertos = this.state.aciertos + this.state.index;
-            console.log(aciertos);
-            var used = this.state.used.map(x => x)
-            used.push(this.state.palabra_index);
-            this.clean();
+    generarCarta(cartas) {
+        var carta = Math.floor(Math.random() * this.n * this.n);
+        while (cartas[carta]) {
+            var carta = Math.floor(Math.random() * this.n * this.n);
+        }
+        return carta;
+    }
+
+    revisar (i) {
+        if (this.state.selected == i) {
+            var activo = this.state.activo.map(x => x);
+            activo[i] = false;
             this.setState({
-                aciertos: aciertos,
-                pregunta: this.state.pregunta + 1,
-                index: null,
-                palabra_index : null,
-                used: used
+                selected: -1,
+                activo: activo
             });
-		}
-	}
+            return;
+        }
+        if (this.state.cartas[this.state.selected] == this.state.cartas[i]) {
+            var done = this.state.done.concat(i).concat(this.state.selected);
+            this.setState({
+                done: done,
+                selected: -1
+            });
+            if (done.length == this.n * this.n) {
+                this.props.terminar(Math.max(0, 100 - (this.state.movimientos - 2) * this.props.nivel * 3));
+            }
+        } else {
+            this.setState({
+                locked: true
+            });
+            setTimeout(() => {
+                var activo = this.state.activo.map(x => x);
+                activo[i] = false;
+                activo[this.state.selected] = false;
+                this.setState({
+                    movimientos: this.state.movimientos + 1,
+                    activo: activo,
+                    selected: -1,
+                    locked: false
+                });
+            }, 1000 * (4 - this.props.nivel));
+        }
+    }
 
 	render() {
-		if (this.state.pregunta >= this.total_preguntas) {
-            var total_aciertos = this.total_preguntas * 4;
-            var porcentaje = Math.round(this.state.aciertos / total_aciertos * 100);
-			      this.props.terminar(porcentaje);
-            return(<div></div>);
-		} else {
-            var palabras;
-            switch (this.props.nivel) {
-                case 1: palabras = ['Pelota', 'Manzana', 'Corazón', 'Sol', 'Luna', 'Nube', 'Bota', 'Pez'];
-                break;
-                case 2: palabras = ['Flor', 'Palmera', 'Árbol', 'Estrella', 'Automóvil', 'Silla', 'Sombrilla'];
-                break;
-                case 3: palabras = ['Playa', 'Ciudad', 'Atardecer', 'Amanecer', 'Deporte', 'Casa', 'Película', 'Granja'];
-                break;
-            }
-            if(this.state.palabra_index == null) {
-              var p = Math.floor((Math.random() * (palabras.length-1)))
-              while (this.state.used.indexOf(p) >= 0) {
-                  p = Math.floor((Math.random() * (palabras.length-1)))
-              }
-              this.state.palabra_index = p
-            }
-
-            var text = palabras[this.state.palabra_index];
-			return (
-				<div>
-					<div className="offset-2 col-8 text-center text-bold">
-                        <h3>{text}</h3> <br />
-                        <canvas id="canvas" width={300} height={300} style={{ background: 'white' }} onMouseDown={this.dibujar} onMouseMove={this.continuarDibujo} onMouseUp={this.clear} onMouseLeave={this.clear} />
-					</div>
-
-                    <div className="alert alert-success row mt-3">
-                        <div className="offset-1 col-2">
-                            <button onClick={() => {this.seleccionar(0)}} className="btn btn-danger">Muy mal</button>
-                        </div>
-
-                        <div className="col-2">
-                            <button onClick={() => {this.seleccionar(1)}} className="btn btn-warning">Mal</button>
-                        </div>
-
-                        <div className="col-2">
-                            <button onClick={() => {this.seleccionar(2)}} className="btn btn-info">Regular</button>
-                        </div>
-
-                        <div className="col-2">
-                            <button onClick={() => {this.seleccionar(3)}} className="btn btn-primary">Bien</button>
-                        </div>
-
-                        <div className="col-2">
-                            <button onClick={() => {this.seleccionar(4)}} className="btn btn-success">Muy bien</button>
-                        </div>
-                    </div>
-
-                    <div className="row mt-3">
-                        <div className="col-2 offset-10">
-                            <button className="btn btn-principal" onClick={this.siguiente}>Siguiente</button>
-                        </div>
-                    </div>
-				</div>
-			);
-		}
-	}
+        var cartas = this.state.cartas.map((carta, i) =>
+            <div style={{ float: 'left', padding: '20px', width: '25%' }}>
+                <img style={{ border: this.state.done.indexOf(i) >= 0 ? 'solid 5px green' : '', cursor: 'pointer', height: '300px', objectFit: 'cover', minHeight: '300px', maxWidth: '100%', width: '100%' }} src={'./img/' + (this.state.activo[i] ? carta : 'tarjeta') + '.png'} onClick={() => {
+                    if (this.state.locked || this.state.done.indexOf(i) >= 0) return;
+                    var activo = this.state.activo.map(x => x);
+                    activo[i] = true;
+                    if (this.state.selected == -1) {
+                        this.setState({
+                            activo: activo,
+                            selected: i
+                        });
+                    } else {
+                        this.setState({
+                            activo: activo
+                        });
+                        this.revisar(i);
+                    }
+                }} />
+            </div>
+        );
+        return (
+            <div style={{ padding: '20px' }}>
+                {cartas}
+            </div>
+        );
+    }
 }
+
 class Instrucciones extends React.Component {
 	render() {
 		return (
