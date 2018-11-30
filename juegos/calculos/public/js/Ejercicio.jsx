3c57945c7fce3014ab9e6c126f91634350295206
Fernando Flores Garcia <fflores10@uabc.edu.mx>
2018-11-30 11:24:34 -0800

    añadiendo generacion dinámica de problemas

--- juegos/calculos/public/js/Ejercicio.jsx
+++ juegos/calculos/public/js/Ejercicio.jsx
@@ -44,11 +44,16 @@ class Ejercicio extends React.Component {
 		return operadores[this.generarAleatorio(0,3)]
 	}
 
+	generarOpcionDificil(num){
+		var min = num - 10
+		var max = num + 10
+		return this.generarAleatorio(min, max)
+	}
+
 	generarEjercicios() {
 		this.ejercicios = [];
 		//config tiene la estructura config[nivel][indice].{tira,respuesta}
 		// En esta parte se selecciona la pregunta
-		var config = this.recuperarConfig()
 		for (var i = 0; i < this.numeroPreguntas; i++) {
 
 			var ejercicio = new Object();
@@ -153,6 +158,45 @@ class Ejercicio extends React.Component {
 					break;
 					//En el nivel difícil se crean problemas escritos utilizando sumas y restas
 				case 3:
+				var operando1 =  this.generarAleatorio(0,10)// numeros de un dígito
+				var operando2 = this.generarAleatorio(0,10)
+				var operador = this.generarOperador()
+				var resultado
+				var enunciado
+				switch (operador) {
+					case '+':
+						resultado = operando1 + operando2
+						enunciado = 'Si tengo ' + operando1 + ' piezas y sumo ' +operando2 + ' piezas. ¿Cuántas tengo?'
+						break;
+					case '-':
+					resultado = operando1 - operando2
+					enunciado = 'Si tengo ' + operando1 + ' piezas y resto ' +operando2 + ' piezas. ¿Cuántas tengo?'
+					break;
+					case 'x': //problemas de
+					resultado = operando1 * operando2
+					enunciado = 'Si tengo ' + operando1 + ' piezas y multiplico ' +operando2 + ' piezas. ¿Cuántas tengo?'
+					break;
+					case '/':// PRoblemas de reparticion
+					while(operando2 == 0) {
+						operando2 = this.generarAleatorio(0,10)
+					}
+					resultado = operando1 / operando2
+					resultado = Number((resultado).toFixed(3))
+					enunciado = 'Si tengo ' + operando1 + ' piezas y reparto ' +operando2 + ' piezas. ¿Cuántas tengo?'
+					break;
+				}
+				ejercicio.pregunta = enunciado
+				ejercicio.respuesta = resultado
+				var opciones = []
+				var j;
+				for(j=0;j < 3; j++) {
+					let opcion = this.generarOpcionDificil(ejercicio.respuesta)
+					if(opciones.includes(opcion) || ejercicio.respuesta == opcion){
+						opcion = this.generarOpcionDificil(ejercicio.respuesta)
+					}
+					opciones.push(opcion)
+				}
+				opciones.push(ejercicio.respuesta)
 					break;
 			}
 
@@ -243,122 +287,5 @@ class Ejercicio extends React.Component {
 				</div>
 			);
 		}
-	}
-
-	recuperarConfig() {
-		var config = {
-			1 : [
-				{
-		      pregunta: "2 + 1 = ",
-		      respuesta: "3",
-					opciones : ["1", "2", "4"]
-		    },
-				{
-		      pregunta: "2 - 1 = ",
-		      respuesta: "1",
-					opciones : ["3", "0", "5"]
-		    },
-				{
-		      pregunta: "7 + 3 = ",
-		      respuesta: "10",
-					opciones : ["13", "73", "7"]
-		    },
-				{
-		      pregunta: "12 + 13 = ",
-		      respuesta: "25",
-					opciones : ["15", "26", "13"]
-		    },
-		    {
-		      pregunta: "5 - 5 = ",
-		      respuesta: "0",
-					opciones : ["1", "10", "14"]
-		    },
-				{
-		      pregunta: "10 + 10 = ",
-		      respuesta: "20",
-					opciones : ["0", "14", "15"]
-		    },
-		    {
-		      pregunta: "3 x 5 = ",
-		      respuesta: "14",
-					opciones : ["15", "13", "14"]
-		    },
-				{
-		      pregunta: "26 - 13 = ",
-		      respuesta: "13",
-					opciones : ["16", "10", "15"]
-		    },
-		    {
-		      pregunta: "26 + 43 = ",
-		      respuesta: "69",
-					opciones : ["72", "70", "67"]
-		    }
-			],
-		  2 : [
-		    {
-		      pregunta: "3 x 5 = ",
-		      respuesta: "15",
-					opciones : ["8", "21", "11"]
-		    },
-		    {
-		      pregunta: "7 x 7 = ",
-		      respuesta: "49",
-					opciones : ["14", "0", "59"]
-		    },
-		    {
-		      pregunta: "60 / 5 = ",
-		      respuesta: "12",
-					opciones : ["3", "10", "30"]
-		    },
-		    {
-		      pregunta: "6 x 5 = ",
-		      respuesta: "30",
-					opciones : ["11", "50", "60"]
-		    },
-		    {
-		      pregunta: "108 / 12 = ",
-		      respuesta: "9",
-					opciones : ["7", "12", "11"]
-		    },
-		    {
-		      pregunta: "36 / 2 = ",
-		      respuesta: "18",
-					opciones : ["38", "75", "11"]
-		    }
-		  ],
-			3 : [
-				{
-		      pregunta: "Tenemos 182 bombones y tenemos que repartirlos entre 26 personas ¿Cuántos bombones le tocan a cada persona?",
-		      respuesta: "7",
-					opciones : ["6", "8", "5"]
-		    },
-		    {
-		      pregunta: "360 / 120 =",
-		      respuesta: "3",
-					opciones : ["2", "120", "1"]
-		    },
-				{
-		      pregunta: "495 / 5 = ",
-		      respuesta: "99",
-					opciones : ["6", "2", "4"]
-		    },
-		    {
-		      pregunta: "11 x 12 = ",
-		      respuesta: "132",
-					opciones : ["112", "114", "120"]
-		    },
-				{
-		      pregunta: "Si una docena de huevos vale 200 pesos. ¿Cuánto valen 5 docenas?",
-		      respuesta: "1000",
-					opciones : ["205", "12", "1200"]
-		    },
-		    {
-		      pregunta: "Si vamos a comprar el mandado y el costo total es de 357, si pagamos con un billete de 500 ¿Cuánto nos sobrará?",
-		      respuesta: "143",
-					opciones : ["153", "413", "103"]
-		    }
-			]
-		}
-		return config
 	}
 }
