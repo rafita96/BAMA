d79c831b8d806060cf7b1c4a9f8ec5b9c4508666
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-09-05 10:56:14 -0700

    refactor(local server): reformat code

--- src/conf.js
+++ src/conf.js
@@ -3,18 +3,16 @@
 */
 
 /*
-    En la parte de database se encuentra el nombre de la base de datos 'volveraempezar', 
+    En la parte de database se encuentra el nombre de la base de datos 'volveraempezar',
     el host 'localhost' y el puerto '27017'.
-    En la parte de session se encuentra el nombre de usuario 'admin', la contraseña 'psicologo', 
-    la colección en donde se almacenan los administradores en la base de datos 'admins' y 
+    En la parte de session se encuentra el nombre de usuario 'admin', la contraseña 'psicologo',
+    la colección en donde se almacenan los administradores en la base de datos 'admins' y
     el número de 'vueltas' que tiene que hacer el algoritmo de cifrado de contraseñas.
 
-    Estos datos se pueden cambiar dependiendo del nombre de usuario y contraseña que se desee, 
+    Estos datos se pueden cambiar dependiendo del nombre de usuario y contraseña que se desee,
     así como se pueden modificar los parámetros de la base de datos.
 */
-
-// Permite cifrar la contraseña para luego compararla
-var crypto = require('crypto');
+const crypto = require('crypto');
 
 var roles = {
     ROLE_ADMIN: 0,
