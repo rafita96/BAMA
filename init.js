4f70bf1925e8e4fa77b3cf2e5b9c0edde57457c0
Carlos Sanchez JR <carlos1eduardo1sanchez1torres@gmail.com>
2020-09-14 20:39:29 -0700

    hotfix-getting-started (#3)
    * chore(docs): README translate to english
    
    * docs(CONTRIBUTORS): add contributors left
    
    * refactor(server): remove unneeded comments
    
    * refactor(local server): reformat code
    
    * refactor(local server): reformat code
    
    * refactor(local server): reformat code
    
    * refactor(local server): reformat code
    
    * chore(local server): add env variables
    
    * chore(local server): add env variables
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(deployment): add heroku remote
    
    * chore(config): es5 to es6
    
    * chore(config): es5 to es6
    
    * chore(config): es5 to es6
    
    * chore(): add deleted images from master

--- init.js
+++ init.js
@@ -1,57 +1,15 @@
-/**
-* @author   Rafael Peralta Blanco <rafael.peralta.blanco@gmail.com>
-*/
+require('dotenv').config();
+const appConfig = require('./src/conf').conf;
+const dbManager = require('./src/controllers/database');
+const {encrypt} = require("./src/conf");
 
-/**
-    Inserta en la base de datos un usuario administrador
-*/
-var crypto = require('crypto');         // Permite cifrar la contraseña
-// Archivo local que contiene la información de la cuenta de administrador
-var appConfig = require('./src/conf').conf;
-// El manejador de la base de datos.
-var dbManager = require('./src/controllers/database');
+const usr = appConfig.users[0];
 
-/* Cifra las contraseñas */
-function encrypt(text){
-  var cipher = crypto.createCipher(appConfig["session"]["algorithm"], appConfig["session"]["secret"])
-  var crypted = cipher.update(text,'utf8','hex')
-  crypted += cipher.final('hex');
-  return crypted;
-}
-
-/*
-    Registra cada usuario predefinido a la base de datos
-*/
-for (var i = 0; i < appConfig.users.length; i++) {
-    var usr = appConfig.users[i];
-
-    // Se cifra la contraseña.
-    var hash = encrypt(usr["password"]);
-
-    // Busca en la base de datos si el usuario ya existe,
-    // Si no existe entonces lo inserta en la collección, sino entonces
-    // no hace algo.
-    dbManager.find(appConfig["session"]["collection"],
-                    {username: usr["username"]}, 
-                        function (res) {
-                            if(res.length == 0){
-                                dbManager.insertar(appConfig["session"]["collection"],
-                                    {
-                                        username: usr["username"],
-                                        password: hash,
-                                        role: usr["role"]
-                                    },
-
-                                function(err){
-                                    if(err){
-                                        console.log("Error")
-                                    }
-                                }
-
-                                );
-                            }  
-                        }
-                    );
-}
-
-
+dbManager.insertar(appConfig["session"]["collection"],
+    {
+        username: usr["username"],
+        password: encrypt(usr["password"]),
+        role: usr["role"]
+    },
+    (e) => {}
+);
