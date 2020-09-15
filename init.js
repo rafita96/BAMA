4241e59b5be51d09382a0acb50c201e0122d1499
Carlos Sanchez <hola@sanchezcarlosjr.com>
2020-09-14 20:52:18 -0700

    chore(config): deployment to production

--- init.js
+++ init.js
@@ -1,42 +1,15 @@
-const crypto = require('crypto');
+require('dotenv').config();
 const appConfig = require('./src/conf').conf;
 const dbManager = require('./src/controllers/database');
+const {encrypt} = require("./src/conf");
 
-function encrypt(text){
-    const cipher = crypto.createCipher(appConfig["session"]["algorithm"], appConfig["session"]["secret"])
-    let crypted = cipher.update(text,'utf8','hex')
-    crypted += cipher.final('hex');
-    return crypted;
-}
+const usr = appConfig.users[0];
 
-for (var i = 0; i < appConfig.users.length; i++) {
-    var usr = appConfig.users[i];
-
-    // Se cifra la contraseña.
-    const hash = encrypt(usr["password"]);
-
-    // Busca en la base de datos si el usuario ya existe,
-    // Si no existe entonces lo inserta en la collección, sino entonces
-    // no hace algo.
-    dbManager.find(appConfig["session"]["collection"],
-        {username: usr["username"]},
-        function (res) {
-            if(res.length == 0){
-                dbManager.insertar(appConfig["session"]["collection"],
-                    {
-                        username: usr["username"],
-                        password: hash,
-                        role: usr["role"]
-                    },
-
-                    function(err){
-                        if(err){
-                            console.log("Error")
-                        }
-                    }
-
-                );
-            }
-        }
-    );
-}
+dbManager.insertar(appConfig["session"]["collection"],
+    {
+        username: usr["username"],
+        password: encrypt(usr["password"]),
+        role: usr["role"]
+    },
+    (e) => {}
+);
